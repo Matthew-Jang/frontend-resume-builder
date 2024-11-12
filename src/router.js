f5cd03a84fc0b0b77355c90151797f11f65d4b18
Jacob Honaker <83312842+JakePath21@users.noreply.github.com>
2024-11-11 21:55:39 -0600

    Added a missing ;

--- src/router.js
+++ src/router.js
@@ -8,7 +8,7 @@ import AddTutorial from "./views/AddTutorial.vue";
 import ViewTutorial from "./views/ViewTutorial.vue";
 import AddLesson from "./views/AddLesson.vue";
 import EditLesson from "./views/EditLesson.vue";
-import ProfileStep from "./views/ProfileStep.vue"
+import ProfileStep from "./views/ProfileStep.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
